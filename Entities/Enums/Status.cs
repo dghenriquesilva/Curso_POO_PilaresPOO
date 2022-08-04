e3e76249931a87e2909a5ba7ea400989f45dfa63
Diogo Henrique <dghenriquesilva@gmail.com>
2022-08-04 18:28:33 -0300

    Adicionada Enumeração Status do aluno (aprovado, reprovado...)

---
+++ Entities/Enums/Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp5.Entities.Enums
+{
+    public enum Status : int
+    {
+        Aprovado = 1,
+        Reprovado = 2,
+        Recuperacao = 3
+    }
+}
--- Estudante.cs
+++ Estudante.cs
@@ -2,20 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using ConsoleApp5.Entities.Enums;
 namespace ConsoleApp5
 {
     public class Estudante : Pessoa
     {
         public string Turma { get; private set; }
         public List<int> Notas { get; set; }
-
+        public Status StatusEstudante { get; set; }
+        public double Media { get; private set; }
         // base --> usando a herança
         public Estudante(string turma, string nome, string documento, DateTime dataNascimento)
             : base(nome, documento, dataNascimento)
         {
             Turma = turma;
-            Notas = new List<int> { 10, 6, 3, 8 };
+            Notas = new List<int> { 10, 6, 3, 1 };
 
         }
         // usando o polimorfismo
@@ -23,7 +24,21 @@ namespace ConsoleApp5
         {
             base.SetApresentar();
             var media = Notas.Average();
-            Console.WriteLine($"Sou um estudante da turma: {Turma}, Média da nota: {media}");
+            Media = 6;
+          if (media >= Media)
+            {
+                StatusEstudante = Status.Aprovado;
+            }
+            else if(media >=5 && media < Media)
+            {
+                StatusEstudante = Status.Recuperacao;
+            }
+            else
+            {
+                StatusEstudante = Status.Reprovado;
+            }
+
+            Console.WriteLine($"Sou um estudante da turma: {Turma}, Média da nota: {media} e a situação é: {StatusEstudante.ToString()}");
         }
 
 
